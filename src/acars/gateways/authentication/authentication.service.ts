44076c8d4204f2a593d43c98f4b2fc404498fc31
Joshua Hughes <joshua@plutonus.dev>
2024-12-08 15:25:50 +0100

    feat: create authentication.service.ts for interfacing with the database

---
+++ src/acars/gateways/authentication/authentication.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/_lib/prisma.service";
+
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private prisma: PrismaService) {}
+}
