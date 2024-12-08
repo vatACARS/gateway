3897dcbb7ed1bd34ab084aa6cffc64059d1d5074
Joshua Hughes <joshua@plutonus.dev>
2024-12-08 23:42:54 +0100

    chore: updated imports for app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,35 @@ import { AppService } from './app.service';
 import { AcarsGateway } from './acars/acars.gateway';
 import { DataModule } from './data/data.module';
 
-import { AuthenticationGateway } from './acars/gateways';
+import { PrismaService } from './_lib/prisma.service';
+import { AgendaService } from './_lib/agenda.service';
+
+import {
+  AuthenticationGateway,
+  StationGateway,
+  MessageGateway,
+  AuthenticationService,
+  StationService,
+  MessageService,
+} from './acars/gateways';
 
 @Module({
   imports: [DataModule],
   controllers: [AppController],
-  providers: [AppService, AcarsGateway, AuthenticationGateway],
+  providers: [
+    AppService,
+    PrismaService,
+    AgendaService,
+
+    AcarsGateway,
+
+    AuthenticationGateway,
+    StationGateway,
+    MessageGateway,
+
+    AuthenticationService,
+    StationService,
+    MessageService,
+  ],
 })
 export class AppModule {}
