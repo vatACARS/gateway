5374213d2193e81c16505f5faca8c244572e0241
Joshua Hughes <joshua@plutonus.dev>
2024-12-08 23:43:25 +0100

    chore: removed unused import in agenda.service.ts

--- src/_lib/agenda.service.ts
+++ src/_lib/agenda.service.ts
@@ -1,13 +1,11 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import Agenda = require('agenda');
 
-import { AuthenticationService } from 'src/acars/gateways/authentication/authentication.service';
-
 @Injectable()
 export class AgendaService implements OnModuleInit, OnModuleDestroy {
   agenda: any;
 
-  constructor(private readonly authenticationService: AuthenticationService) {
+  constructor() {
     this.agenda = new Agenda.Agenda({
       db: { address: process.env.database_url, collection: 'tasks' },
     });
