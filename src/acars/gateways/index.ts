0a75cc522a7f05122d6bc09742050d120a27bbae
Joshua Hughes <joshua@plutonus.dev>
2024-12-08 23:45:58 +0100

    chore: updated exports for gateways index.ts

--- src/acars/gateways/index.ts
+++ src/acars/gateways/index.ts
@@ -1,1 +1,17 @@
 export * from './authentication/authentication.gateway';
+export * from './station/station.gateway';
+export * from './message/message.gateway';
+
+export * from './authentication/authentication.service';
+export * from './station/station.service';
+export * from './message/message.service';
+
+import * as AuthenticationEnums from './authentication/authentication.enums';
+import * as StationEnums from './station/station.enums';
+import * as MessageEnums from './message/message.enums';
+
+export const Enum = {
+  ...AuthenticationEnums,
+  ...StationEnums,
+  ...MessageEnums,
+};
