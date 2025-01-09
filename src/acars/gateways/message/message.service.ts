378db97e3e83a4070e9cfaaab847f39aae55f60a
Joshua Hughes <joshua@plutonus.dev>
2025-01-09 23:47:04 +0100

    refactor: enhance message sending logic by updating transmission count and improving fetch handling in MessageService

--- src/acars/gateways/message/message.service.ts
+++ src/acars/gateways/message/message.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/services/prisma.service';
 import { ClientsService } from 'src/services/clients.service';
 import { createResponse } from 'src/_lib/apiResponse';
-import { v4 as uuidv4 } from 'uuid';
 import { AuthorityAction, AuthorityCategory } from './message.enums';
 
 @Injectable()
@@ -26,6 +25,17 @@ export class MessageService {
       const senderStation = await this.validateSender(sender);
       const receiverStation = await this.findReceiver(recipient);
 
+      await this.prisma.station.update({
+        where: {
+          acarsUser: sender,
+        },
+        data: {
+          txCount: {
+            increment: 1,
+          },
+        },
+      });
+
       await this.prisma.message.create({
         data: {
           senderUser: {
@@ -45,36 +55,39 @@ export class MessageService {
         include: { oauthAccounts: true },
       });
 
-      const hoppiesToken = user.oauthAccounts.find(o => o.provider === 'hoppies')?.accessToken;
+      const hoppiesToken = user.oauthAccounts.find(
+        (o) => o.provider === 'hoppies',
+      )?.accessToken;
 
-      if(hoppiesToken) {
-        const data = await fetch(
-          'http://www.hoppie.nl/acars/system/connect.html',
-          {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: new URLSearchParams({
-              logon: hoppiesToken,
-              from: senderStation.logonCode,
-              to: recipient,
-              type: 'CPDLC',
-              packet: '/data2/1/1/N/TEST',
-            }),
+      if (hoppiesToken)
+        await fetch('http://www.hoppie.nl/acars/system/connect.html', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: new URLSearchParams({
+            logon: hoppiesToken,
+            from: senderStation.logonCode,
+            to: recipient,
+            type: 'CPDLC',
+            packet: `/data2/${senderStation.txCount + 1}/${replyToId}/${responseCode}/${message}`,
+          }),
+        });
+
+      await this.sendToRecipientSocket(
+        recipient,
+        AuthorityCategory.CPDLC,
+        AuthorityAction.ReceiveCPDLCMessage,
+        {
+          cpdlc: {
+            sender: senderStation.logonCode,
+            responseCode,
+            replyToId,
+            message,
           },
-        );
-      }
-
-      await this.sendToRecipientSocket(recipient, AuthorityCategory.CPDLC, AuthorityAction.ReceiveCPDLCMessage, {
-        cpdlc: {
-          sender: senderStation.logonCode,
-          responseCode,
-          replyToId,
-          message,
         },
-      });
+      );
 
       return { success: true, message: 'Message sent' };
-    } catch(err) {
+    } catch (err) {
       return { success: false, message: err.message };
     }
   }
@@ -105,9 +118,11 @@ export class MessageService {
         include: { oauthAccounts: true },
       });
 
-      const hoppiesToken = user.oauthAccounts.find(o => o.provider === 'hoppies')?.accessToken;
+      const hoppiesToken = user.oauthAccounts.find(
+        (o) => o.provider === 'hoppies',
+      )?.accessToken;
 
-      if(hoppiesToken) {
+      if (hoppiesToken) {
         const data = await fetch(
           'http://www.hoppie.nl/acars/system/connect.html',
           {
@@ -124,24 +139,29 @@ export class MessageService {
         );
       }
 
-      await this.sendToRecipientSocket(recipient, AuthorityCategory.Telex, AuthorityAction.ReceiveTelexMessage, {
-        telex: {
-          sender: senderStation.logonCode,
-          message,
+      await this.sendToRecipientSocket(
+        recipient,
+        AuthorityCategory.Telex,
+        AuthorityAction.ReceiveTelexMessage,
+        {
+          telex: {
+            sender: senderStation.logonCode,
+            message,
+          },
         },
-      });
+      );
 
       return { success: true, message: 'Message sent' };
-    } catch(err) {
+    } catch (err) {
       return { success: true, message: err.message };
     }
   }
 
   private async validateSender(sender: string) {
     const senderStation = await this.prisma.station.findUnique({
-      where: { acarsUser: sender }
+      where: { acarsUser: sender },
     });
-    if(!senderStation) throw new Error("Invalid sender");
+    if (!senderStation) throw new Error('Invalid sender');
     return senderStation;
   }
 
@@ -157,14 +177,15 @@ export class MessageService {
     action: AuthorityAction,
     payload: Record<string, any>,
   ) {
-    const recipientSocket = this.clientsService.getClientByStationCode(recipient);
-    if(!recipientSocket) return;
+    const recipientSocket =
+      this.clientsService.getClientByStationCode(recipient);
+    if (!recipientSocket) return;
 
     recipientSocket.send(
       createResponse('success', 'gateway', '', {
         gateway,
         action,
-        ...payload
+        ...payload,
       }),
     );
   }
